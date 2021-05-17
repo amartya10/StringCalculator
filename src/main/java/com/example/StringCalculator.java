bb46a4cdbb8cf9403941335012b36acc51772875
amartya <amartyathorat@gmail.com>
2021-05-17 19:36:38 +0530

    reformatting + comment

--- src/main/java/com/example/StringCalculator.java
+++ src/main/java/com/example/StringCalculator.java
@@ -19,17 +19,22 @@ public class StringCalculator
 
     public int add(final String numbers) {
         if (numbers.equals("")) return 0;
+        //default delimiters "\n" ","
         StringBuilder regex =  new StringBuilder("\n|,");
-        String numbersSeparatedByDelimiterStartEnd = numbers;
+        //temp storing numbers (assuming delimiter not specified)
+        String numbersSeparatedByDelimitersExpression = numbers;
         if (numbers.startsWith(START_DELIMITER_WITH)){
             int startIndexDelimiter = START_DELIMITER_WITH.length();
             int endIndexDelimiter = numbers.indexOf(END_DELIMITER_WITH);
-            String delimiters = numbers.substring(startIndexDelimiter,endIndexDelimiter);
-            Arrays.stream(getDelimiters(delimiters)).
+            //get delimiters substring
+            String delimitersExpression = numbers.substring(startIndexDelimiter,endIndexDelimiter);
+            //append regex  delimiters
+            Arrays.stream(getDelimiters(delimitersExpression)).
                     forEach(delimiter -> regex.append("|"+Pattern.quote(delimiter)) );
-            numbersSeparatedByDelimiterStartEnd = numbers.substring(endIndexDelimiter + 1);
+
+            numbersSeparatedByDelimitersExpression = numbers.substring(endIndexDelimiter + END_DELIMITER_WITH.length());
         }
-        String [] numbersSeparated = numbersSeparatedByDelimiterStartEnd.split(regex.toString());
+        String [] numbersSeparated = numbersSeparatedByDelimitersExpression.split(regex.toString());
         return  _add(numbersSeparated);
     }
 
@@ -42,25 +47,39 @@ public class StringCalculator
         return  negativeNumbers.toString() ;
     }
     public String  [] getDelimiters(String delimiters){
+        /*
+            step 1 if multiple delimiters replace "]["(followed by) to "|"
+            step 2 replace "[" , "]" to ""
+            step 3 split by |
+            Example
+                "[::][;;]"
+                "[::|;;]"
+                "::|;;"
+                return ["::",";;"]
+        */
+       
         return  delimiters
-                .replaceAll("]\\[","|")
-                .replaceAll("[\\[\\]]","")
-                .split("[\\|]");
+                .replaceAll("]\\[","|")    //
+                .replaceAll("[\\[\\]]","") //
+                .split("[\\|]");              //
     }
 
     public int _add(String []  numbersSeparated) {
+
         AtomicReference<Integer> sum = new AtomicReference<>(0);
+
         IntStream.range(0,numbersSeparated.length)
-                .forEach(index -> {
-                    Integer number = Integer.parseInt(numbersSeparated[index]);
-                    if (number < 0 ){
-                        String negativeNumbers = getNegativeNumbers(Arrays.copyOfRange( numbersSeparated,index,numbersSeparated.length));
-                        throw  new IllegalArgumentException("Negative Number Not Allowed " + negativeNumbers);
-                    }
-                    if (number <= 1000){
-                        sum.updateAndGet(v -> v + number);
-                    }
-                });
+            .forEach(index -> {
+                Integer number = Integer.parseInt(numbersSeparated[index]);
+                if (number < 0 ){
+                    String negativeNumbers = getNegativeNumbers(Arrays.copyOfRange( numbersSeparated,index,numbersSeparated.length));
+                    throw  new IllegalArgumentException("Negative Number Not Allowed " + negativeNumbers);
+                }
+                if (number <= 1000){
+                    sum.updateAndGet(v -> v + number);
+                }
+            });
+                
         return  sum.get();
     }
 
